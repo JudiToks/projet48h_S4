8db4dfdd9880178428b0090bf0a46deb2d31d567
Nomena Lebreton <115571582+ITNomena@users.noreply.github.com>
2023-07-11 13:58:00 +0300

    ok ok !

--- LoginP2_ProfilP2/Profil1/js/profil.js
+++ LoginP2_ProfilP2/Profil1/js/profil.js
@@ -17,6 +17,10 @@
                 required: true,
                 digits: true,
             },
+            adresse: {
+                required: true,
+                digits: true
+            }
         },
         messages: {
             poids: {
@@ -29,24 +33,24 @@
                 required: "Veuillez entrer votre âge",
                 digits: "Veuillez entrer une valeur numérique",
             },
+            adresse: {
+                required: "Veuillez indiquer un lieu valide",
+            
+            },
         },
         onfocusout: function(element) {
             $(element).valid();
         },
         highlight: function(element, errorClass, validClass) {
             $(element)
-                .parent()
-                .parent()
-                .find(".form-group")
+                .closest('.form-group')
                 .addClass("form-error");
             $(element).removeClass("valid");
             $(element).addClass("error");
         },
         unhighlight: function(element, errorClass, validClass) {
             $(element)
-                .parent()
-                .parent()
-                .find(".form-group")
+                .closest('.form-group')
                 .removeClass("form-error");
             $(element).removeClass("error");
             $(element).addClass("valid");
@@ -58,26 +62,13 @@
         bodyTag: "fieldset",
         transitionEffect: "fade",
         labels: {
-            previous: "Précédent",
-            next: "Suivant",
             current: "",
         },
-        titleTemplate: '<h3 class="title">#title#</h3>',
+        titleTemplate: '<h3 class="title">#signup-form-h-0#</h3>',
         onInit: function(event, currentIndex) {
             if (currentIndex === 0) {
                 form.find(".actions").addClass("test");
             }
-        },
-        onStepChanging: function(event, currentIndex, newIndex) {
-            form.validate().settings.ignore = ":disabled,:hidden";
-            return form.valid();
-        },
-        onFinishing: function(event, currentIndex) {
-            form.validate().settings.ignore = ":disabled";
-            return form.valid();
-        },
-        onStepChanged: function(event, currentIndex, priorIndex) {
-            // Code to be executed after each step change (if needed)
         },
     });
 
@@ -105,6 +96,4 @@
             reader.readAsDataURL(input.files[0]);
         }
     }
-
-    $("#submit-button").attr("href", "page rediriger");
 })(jQuery);
